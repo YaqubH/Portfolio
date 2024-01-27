43f22d7649e5c16ae5e103aa8a5b9fe1d18a3a54
YaqubH <Yaqubhasann@gmail.com>
2024-01-27 12:29:39 -0800

    Mobile Optimization
    Added features that optimized the site for mobile use.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,30 +1,61 @@
-import { Link, NavLink } from 'react-router-dom';
-import './index.scss';
-import LogoS from '../../assets/images/logo-s.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser, faSuitcase } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './index.scss'
+import { useState } from 'react'
+import LogoS from '../../assets/images/logo-s.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope, faSuitcase, faBars, faClose } from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
 
-const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt="logo" />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
-                <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-            </NavLink>
-        </nav>
-        <ul>
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <div className="nav-bar">
+      <Link 
+        className="logo"
+        to="/"
+        onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt="Logo" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink 
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink 
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon 
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className='close-icon' />
+      </nav>
+      <ul>
             <li>
                 <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/yaqub-hasan/" className="icon">
                     <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
@@ -38,7 +69,14 @@ const Sidebar = () => (
                 </a>
             </li>
         </ul>
+      <FontAwesomeIcon 
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ffd700"
+          size="3x"
+          className='hamburger-icon' />
     </div>
-);
+  )
+}
 
-export default Sidebar;
+export default Sidebar
