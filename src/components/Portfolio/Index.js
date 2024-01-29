6ea2edcc5b7bc9ee025aa4089d22512b3ebb3225
YaqubH <Yaqubhasann@gmail.com>
2024-01-29 01:33:23 -0800

    Update

--- src/components/Portfolio/Index.js
+++ src/components/Portfolio/Index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
-import "./index.scss";
+import './index.scss'
 import { getDocs, collection } from 'firebase/firestore';
 import { db } from '../../firebase';
 
